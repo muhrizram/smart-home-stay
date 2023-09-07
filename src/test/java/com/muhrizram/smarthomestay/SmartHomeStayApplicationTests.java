1174def7b234f42ce6a7a4bb3b66482ee534ea06
muhrizram <muhrizram19@gmail.com>
2023-09-07 16:34:39 +0700

    First Commit

---
+++ src/test/java/com/muhrizram/smarthomestay/SmartHomeStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhrizram.smarthomestay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
