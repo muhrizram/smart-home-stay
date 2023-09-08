7f4ea89aeeda8ea13300e211b34faf98e734d4cb
muhrizram <muhrizram19@gmail.com>
2023-09-08 18:53:52 +0700

    HTTP PUT Method

--- src/main/java/com/muhrizram/smarthomestay/controller/UserController.java
+++ src/main/java/com/muhrizram/smarthomestay/controller/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,6 +40,12 @@ public class UserController {
     @PostMapping("/users")
     public String saveUser(@RequestBody User user) {
         return "Saving the user details to the database " + user;
+    }
+
+    @PutMapping("/users/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        System.out.println("Updating the user data for the id " + id);
+        return user;
     }
 
     @DeleteMapping("/users")
